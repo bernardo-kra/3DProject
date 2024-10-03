0dabb709e4bd9f03b88bb04b233dd805f5e6165b
Bernardo k <youremail@example.com>
2024-10-03 17:56:11 -0300

    CU3D7 - Add routes

--- 3DModel/src/App.jsx
+++ 3DModel/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import "./index.css"
 import ModelViewer from './components/3DModelViewer'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
 
   return (
-    <>
-    <div>
-      <ModelViewer></ModelViewer>
-    </div>
-    </>
+    <Routes>
+      <Route path="/" element={<div>home</div>} />
+      <Route path="/project/:id" element={<div>project</div>} />
+      <Route path="/my-project/:id" element={<div>my-project</div>} />
+      <Route path="/my-projects" element={<ModelViewer/>} />
+      <Route path='create-project' element={<div>create-project</div>} />
+      <Route path="/register" element={<div>register</div>} />
+      <Route path="/login" element={<div>login</div>} />
+      <Route path="/about" element={<div>about</div>} />
+      <Route path="/contact" element={<div>contact</div>} />
+    </Routes>
   )
 }
-
 export default App
